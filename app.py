dccb0570e51ce4fa8043de5dfef96a0f8aa0a07a
Your Name <caitlinstanton44@gmail.com>
2015-11-19 11:35:09 -0500

    C

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-"""
 from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
@@ -10,61 +9,4 @@ def home():
 if __name__ == "__main___":
     app.debug = True
     app.secret_key = "this is a secret key"
-    app.run()
-"""
-
-from flask import Flask, render_template,request
-
-app = Flask(__name__)
-
-#Variables
-nameForm = True
-name = ''
-d = {'welcome': '',
-     'nameForm':nameForm,
-     'name':name}
-
-@app.route("/home")
-@app.route("/home/")
-@app.route("/")
-def home():
-    nameForm = True
-    if name == '' :
-        nameForm = True
-    return render_template("index.html")
-
-@app.route("/name", methods=['POST'])
-def get_name():
-    name = request.form['nameform']
-    d['name'] = name
-    d['welcome'] = 'Welcome ' + name
-    nameForm = False
-    d['nameForm'] = nameForm
-    return render_template("home.html",d=d)
-
-
-@app.route("/cname", methods=['POST'])
-def change_name():
-    name = request.form['cname']
-    d['name'] = name
-    d['welcome'] = 'Welcome ' + name    
-    nameForm = False
-    d['nameForm']=nameForm
-    return render_template("home.html",d=d)
-
-@app.route("/photography")
-@app.route("/photography/")
-@app.route("/photo")
-@app.route("/photo/")
-def photography():
-    return render_template("photo.html")
-
-@app.route("/about")
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-
-if __name__ == "__main__":
-    app.debug = True
     app.run()
