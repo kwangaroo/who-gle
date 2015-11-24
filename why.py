9d762e2a40f56622a584082e373d672d8a63f356
Kathy Wang <k.wang6898@gmail.com>
2015-11-23 22:02:15 -0500

    going on some dates

--- why.py
+++ why.py
@@ -71,3 +71,45 @@ TODO
 '''
 
 print regNames("Mike Zamansky or is it Mr. Mike Zamansky or is it Mr. Dyrland-Weaver or is it Mike Roft Zamansky or is it Mike Mike Zamansky Zamansky")
+
+
+### WHEN
+
+def getDates(text):
+    """HEY KATHY, DONT BE A DUNCE AND FIX THE REGEX EXPRESSION AND STICK IT WITH SOME IFS AND RUN CONVERT DATE OKAY
+    Given url text, returns list of dates that appear in it.
+    Also, should ignore invalid dates."""
+    exp = "(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}"
+    #validates for mm-dd-yyyy format as of now
+    allDates = re.findall(exp,text)
+        L = []
+    for r in result:
+        L.append(r[0])
+    return L
+
+def convertDates(L):
+    """ Converts a date to the same format: mm/dd/yyyy 
+    Returns modified list."""
+    return L 
+
+def countDates(urls):
+    """ Returns a dictionary with the date mentioned and the number of times it pops up."""
+    ct = {}
+    return ct 
+
+
+"""to accomodate for: 
+   mm-dd-yyyy
+   mm-dd-yy
+   yyyy-mm-dd
+   ^^those with / instead of -
+   Month Day, Year 
+   Perhaps MMM DD, YYYY (Apr 20, 2016) 
+ 
+THIS REGEX IS GONNA BE TERRIBS
+
+Invalid Dates I May Or May Not Have To Care About: 
+Feb 29th for non-leap years (
+31st of feb, apr, jun, sept, nov 
+"""
+   
